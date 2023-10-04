a9a4d0f9db4daa72514dc75484e25030f147bdb5
Matteo <matteo.feyaerts@gmail.com>
2023-10-04 18:19:03 +0200

    jsons for a lot

--- src/main/java/ugi/more_layered_blocks/MoreLayeredBlocksDataGenerator.java
+++ src/main/java/ugi/more_layered_blocks/MoreLayeredBlocksDataGenerator.java
@@ -3,11 +3,13 @@ package ugi.more_layered_blocks;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import ugi.more_layered_blocks.datagen.ModModelProvider;
+import ugi.more_layered_blocks.datagen.ModRecipeProvider;
 
 public class MoreLayeredBlocksDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
---
+++ src/main/java/ugi/more_layered_blocks/datagen/ModRecipeProvider.java
@@ -0,0 +1,49 @@
+package ugi.more_layered_blocks.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.advancement.criterion.Criterion;
+import net.minecraft.block.Blocks;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
+import ugi.more_layered_blocks.block.ModBlocks;
+import ugi.more_layered_blocks.item.ModItems;
+
+import java.util.function.Consumer;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    //examples:
+    ////https://github.com/Tutorials-By-Kaupenjoe/Fabric-Tutorial-1.20.X/blob/a1ce57390adb5ef0f1cd6ba7a0bf6b1b659074f5/src/main/java/net/kaupenjoe/tutorialmod/datagen/ModRecipeProvider.java
+    public ModRecipeProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generate(Consumer<RecipeJsonProvider> exporter) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.LAYERED_DIRT.asItem(), 1)
+                .pattern("###")
+                .input('#', ModItems.DIRT_PILE)
+                .criterion(hasItem(ModItems.DIRT_PILE), conditionsFromItem(ModItems.DIRT_PILE))
+                .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.LAYERED_DIRT)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.LAYERED_SAND.asItem(), 2)
+                .pattern("###")
+                .input('#', ModItems.SAND_PILE)
+                .criterion(hasItem(ModItems.SAND_PILE), conditionsFromItem(ModItems.SAND_PILE))
+                .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.LAYERED_SAND)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.LAYERED_GRAVEL.asItem(), 2)
+                .pattern("###")
+                .input('#', ModItems.GRAVEL_PILE)
+                .criterion(hasItem(ModItems.GRAVEL_PILE), conditionsFromItem(ModItems.GRAVEL_PILE))
+                .offerTo(exporter, new Identifier(getRecipeName(ModBlocks.LAYERED_GRAVEL)));
+
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, ModItems.DIRT_PILE, RecipeCategory.BUILDING_BLOCKS, Blocks.DIRT);
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, ModItems.SAND_PILE, RecipeCategory.BUILDING_BLOCKS, Blocks.SAND);
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.MISC, ModItems.GRAVEL_PILE, RecipeCategory.BUILDING_BLOCKS, Blocks.GRAVEL);
+    }
+}
